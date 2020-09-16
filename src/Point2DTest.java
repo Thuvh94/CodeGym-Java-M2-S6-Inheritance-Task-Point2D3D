c7495d8e8f76437ba27054dc01fc1369d716b24d
Thuvh94 <aloha.hat.2508@gmail.com>
2020-09-16 11:32:18 +0700

    Exercise: Create superclass 2D point and subclass 3D point

---
+++ src/Point2DTest.java
@@ -0,0 +1,9 @@
+public class Point2DTest {
+    public static void main(String[] args) {
+        Point2D firstPoint2D = new Point2D();
+        System.out.println(firstPoint2D);
+
+        Point2D secondPoint2D = new Point2D(2.5f, 3.5f);
+        System.out.println(secondPoint2D);
+    }
+}
---
+++ src/Point3DTest.java
@@ -0,0 +1,9 @@
+public class Point3DTest {
+    public static void main(String[] args) {
+        Point3D firstPoint3D = new Point3D();
+        System.out.println(firstPoint3D);
+
+        Point3D secondPoint3D = new Point3D(2.5f,5.7f,8.7f);
+        System.out.println(secondPoint3D);
+    }
+}
